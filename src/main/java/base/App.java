de19c3c11d38be88c8df872fdce3b2ad3c9dacd1
fshofiquel <fshofique@gmail.com>
2021-06-11 19:04:10 -0400

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,12 @@
+package base;/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Fazlur Shofiquel
+ */
+
+public class App
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
